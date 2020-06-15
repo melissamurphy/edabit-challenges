a6b363e38c8fe380588afee70734c306ae87cb26
melissamurphy <flylittlefeather@yahoo.com>
2020-06-15 06:57:47 +0000

    Robin FizzBuzzFuzz challenge

--- math.js
+++ math.js
@@ -24,3 +24,15 @@ function intWithinBounds(n, lower, upper) {
 // false if n is not an integer
 // false if n equals or exceeds upper
 // false if n is less than lower
+
+function fizzBuzzBuzz(num1, num2) {
+  for(var x = num1; num1 <= num2; num1++) {
+    if(num1%3===0){
+      console.log("Fizz");
+    } else if (num1%5===0) {
+      console.log("Buzz");
+    } else if (num1%7===0) {
+      console.log("Fuzz");
+    }
+  }
+}
