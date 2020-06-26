7b43e4ab08d708cb6bc36fe7c83c10c195a6106e
melissamurphy <flylittlefeather@yahoo.com>
2020-06-26 06:59:03 +0000

    myRobin remove flies string challenge

--- strings.js
+++ strings.js
@@ -39,3 +39,19 @@ function isJackpot(array) {
   }
   return true;
 }
+
+// myRobin: Why don't you do the same: write a function that removes each "fly" you see in a string, as well as any letter from the word "fly" (f, l, or y), just to be safe!
+
+function removeFly(string){
+  var newString = "";
+  for(var i=0; i < string.length; i++){
+    if (string[i]!=="f"&& string[i]!=="l"&&string[i]!=="y"){
+      newString = newString + string[i];
+    }
+  }
+  if(string===newString){
+    return "No flies here!"
+  } else {
+    return "gone:" + newString;
+  }
+}
