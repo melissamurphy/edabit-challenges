7f8950c7d3d3981a771a26c8aa8f2f01a9ebe24e
melissamurphy <flylittlefeather@yahoo.com>
2020-07-03 05:56:16 +0000

    Completed myRobin challenge

--- arrays.js
+++ arrays.js
@@ -23,3 +23,19 @@ function resortArray(array) {
   return jointArray;
 }
 console.log(resortArray(["j1", "j2", "j3", "j4", "j5", "k1", "k2", "k3", "k4", "k5"]));
+
+// Write a function that, when given a list of integers, prints the pair of numbers with the shortest number of steps in between. In this case, you can assume that the array of points is already sorted. For example, if S = {1, 3, 4, 8, 13, 17, 20} is given, the result will be (3, 4).
+
+function shortestStep(array) {
+  var shortestPoints = "(" + array[0] + ", " + array[1] + ")";
+  var leastDiff = array[1] - array[0];
+  for (var i = 1; i < array.length - 1; i++) {
+    if (array[i + 1] - array[i] < leastDiff) {
+      leastDiff = array[i + 1] - array[i];
+      shortestPoints = "(" + array[i] + ", " + array[i + 1] + ")";
+    }
+  }
+  return shortestPoints;
+}
+
+console.log(shortestStep([1, 3, 4, 8, 13, 17, 20]))
