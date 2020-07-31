2975c1d315c1d8d9c13a8a8154fa1cec849b2d74
Melissa Murphy <flylittlefeather@yahoo.com>
2020-07-31 06:51:43 +0000

    First part of myRobin palindrome challenge

--- math.js
+++ math.js
@@ -100,3 +100,18 @@ function remainder(num1, num2) {
 
     console.log(thanos([2, 3, 1, 6, 5, 7]));
 
+    //Palindromes come up a lot during technical interviews. A numerical palindrome is a number that reads the same in reverse as it reads normally. Can you remember how to write a function that returns the closest palindrome to any number of your choice? If your number is exactly in between two palindromes, return the smaller number. If the number you chose IS a palindrome, return itself. Have fun!
+
+    function palindrome(num)
+    function reverse(digits) {
+      var reversed = '';
+      digits = digits + '';
+      for(var i=digits.length-1; i >=0; i--){
+        reversed = reversed + digits[i];
+      }
+    }
+    {
+      for(var i=1; i<10; i++) {
+        if
+      }
+    }
