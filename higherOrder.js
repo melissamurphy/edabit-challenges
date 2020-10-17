a932adaed1d76b5570e4cc158f5ae2e5933acb03
Melissa Murphy <flylittlefeather@yahoo.com>
2020-10-17 06:52:41 +0000

    Completed challenge to pass in parameterless functions as arguments and compare which returns the larger number

--- higherOrder.js
+++ higherOrder.js
@@ -23,3 +23,16 @@ class fifo {
     this.queue.shift(element);
   }
 }
+
+// https://edabit.com/challenge/o7TwicAHWuMkjbDqQ
+// Your function will be passed two functions, f and g, that don't take any parameters. Your function has to call them, and return a string which indicates which function returned the larger number.
+function whichIsLarger(f, g) {
+  if (f() > g()) {
+    return "f";
+  } else if (g() > f()) {
+    return "g";
+  }
+  else {
+    return "neither";
+  }
+}
