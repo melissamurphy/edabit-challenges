24cd0c4f9e0f6ade6e27dab21e7d92c6fd6d2d25
melissamurphy <flylittlefeather@yahoo.com>
2021-01-04 07:55:35 +0000

    Prevented changes to object via Object.freeze() method that freezes object and its prototype

--- arrays.js
+++ arrays.js
@@ -414,3 +414,17 @@ function determineWhoCursedTheMost(obj) {
       return myWins > spouseWins ? "ME!" : spouseWins > myWins ? "SPOUSE!" : "NOBODY!";
 
     }
+
+// Burglary Series(19): Prevent Changes
+// The police sends you an electronic statement for you to sign.As you begin to sign, an error pops up.Apparently, they sent you a protected document.
+// This challenge is a bit different as the function you are given already contains some code that you should not change or remove.Also, don't do a return statement, it is already included. Your task is, given an object, prevent changes to that object.
+function preventChanges(obj) {
+      // write your code here
+      // don't use a return statement
+
+  Object.freeze(obj);
+      // DON'T CHANGE OR REMOVE THE LINES BELOW
+  obj.noChanges = false;
+  obj.signature = "whatever";
+  return obj;
+}
