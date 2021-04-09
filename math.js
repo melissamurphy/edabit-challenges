4246300f40816c7806f4213cb3f296d91ba27350
melissamurphy <flylittlefeather@yahoo.com>
2021-04-09 06:54:03 +0000

    Edited out unfinished code

--- math.js
+++ math.js
@@ -116,19 +116,20 @@ function remainder(num1, num2) {
 
     //Palindromes come up a lot during technical interviews. A numerical palindrome is a number that reads the same in reverse as it reads normally. Can you remember how to write a function that returns the closest palindrome to any number of your choice? If your number is exactly in between two palindromes, return the smaller number. If the number you chose IS a palindrome, return itself. Have fun!
 
-    function palindrome(num)
-    function reverse(digits) {
-      var reversed = '';
-      digits = digits + '';
-      for(var i=digits.length-1; i >=0; i--){
-        reversed = reversed + digits[i];
-      }
-    }
-    {
-      for(var i=1; i<10; i++) {
-        if
-      }
-    }
+    // function palindrome(num)
+
+    // function reverse(digits) {
+    //   var reversed = '';
+    //   digits = digits + '';
+    //   for(var i=digits.length-1; i >=0; i--){
+    //     reversed = reversed + digits[i];
+    //   }
+    // }
+    // {
+    //   for(var i=1; i<10; i++) {
+    //     if
+    //   }
+    // }
 
 // myRobin
 // Sheltered at home, you are so bored out of your mind that you start thinking about palindromes.A palindrome, in our case, is a number that reads the same in reverse as it reads normally.Robin challenges you to write a function that returns the closest palindrome to any number of your choice.If your number is exactly in between two palindromes, return the smaller number.If the number you chose IS a palindrome, return itself.Have fun!
